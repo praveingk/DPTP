e00a831b807941e17dc3c10fa401a315ac2f92e6
Your Name <you@example.com>
2019-07-11 16:25:38 +0800

    Added support for SDE 8.9.1 and 8.8.1

--- CP/run.sh
+++ CP/run.sh
@@ -1,2 +1,18 @@
-source set_bash;
-make clean ; make; ./dptp_topo_cp
+#!/bin/bash
+
+if [ $# -eq 1 ];then
+    use_old=true
+else
+    use_old=false
+fi
+
+if [ $use_old == "true" ];then
+    makefile="Makefile.old"
+    setbash="set_bash.old"
+else
+    makefile="Makefile"
+    setbash="set_bash"
+fi
+
+source $setbash;
+make -f $makefile clean ; make -f $makefile; ./dptp_topo_cp
