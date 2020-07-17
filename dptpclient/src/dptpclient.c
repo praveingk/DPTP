1251393c2f97c8be1aec32a7721156529e97669d
praveingk <praveingk@gmail.com>
2020-07-17 18:12:00 +0800

    Beautify code

--- dptpclient/src/dptpclient.c
+++ dptpclient/src/dptpclient.c
@@ -19,25 +19,22 @@
 #define RX_RING_SIZE 1024
 #define TX_RING_SIZE 1024
 
-#define NUM_MBUFS            8191
-#define MBUF_CACHE_SIZE       250
-
+#define NUM_MBUFS 8191
+#define MBUF_CACHE_SIZE 250
 
 struct rte_mempool *mbuf_pool;
 uint32_t ptp_enabled_port_mask;
 uint16_t ptp_enabled_port_nb;
 static uint8_t ptp_enabled_ports[RTE_MAX_ETHPORTS];
 
-
 static const struct rte_eth_conf port_conf_default = {
-	.rxmode = {
-		.max_rx_pkt_len = RTE_ETHER_MAX_LEN,
-	},
+    .rxmode = {
+        .max_rx_pkt_len = RTE_ETHER_MAX_LEN,
+    },
 };
 
-static const struct  rte_ether_addr tor_switch = {
-	.addr_bytes = {0xa0, 0x00, 0x00, 0x20, 0x00, 0x0a}
-};
+static const struct rte_ether_addr tor_switch = {
+    .addr_bytes = {0xa0, 0x00, 0x00, 0x20, 0x00, 0x0a}};
 
 /*
  * Initializes a given port using global settings and with the RX buffers
@@ -46,89 +43,95 @@ static const struct  rte_ether_addr tor_switch = {
 static inline int
 port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 {
-	struct rte_eth_dev_info dev_info;
-	struct rte_eth_conf port_conf = port_conf_default;
-	const uint16_t rx_rings = 1;
-	const uint16_t tx_rings = 1;
-	int retval;
-	uint16_t q;
-	uint16_t nb_rxd = RX_RING_SIZE;
-	uint16_t nb_txd = TX_RING_SIZE;
+  struct rte_eth_dev_info dev_info;
+  struct rte_eth_conf port_conf = port_conf_default;
+  const uint16_t rx_rings = 1;
+  const uint16_t tx_rings = 1;
+  int retval;
+  uint16_t q;
+  uint16_t nb_rxd = RX_RING_SIZE;
+  uint16_t nb_txd = TX_RING_SIZE;
 
-	if (!rte_eth_dev_is_valid_port(port))
-		return -1;
+  if (!rte_eth_dev_is_valid_port(port))
+    return -1;
 
-	retval = rte_eth_dev_info_get(port, &dev_info);
-	if (retval != 0) {
-		printf("Error during getting device (port %u) info: %s\n",
-				port, strerror(-retval));
+  retval = rte_eth_dev_info_get(port, &dev_info);
+  if (retval != 0)
+  {
+    printf("Error during getting device (port %u) info: %s\n",
+           port, strerror(-retval));
 
-		return retval;
-	}
+    return retval;
+  }
 
-	if (dev_info.tx_offload_capa & DEV_TX_OFFLOAD_MBUF_FAST_FREE)
-		port_conf.txmode.offloads |=
-			DEV_TX_OFFLOAD_MBUF_FAST_FREE;
-	/* Force full Tx path in the driver, required for IEEE1588 */
-	port_conf.txmode.offloads |= DEV_TX_OFFLOAD_MULTI_SEGS;
+  if (dev_info.tx_offload_capa & DEV_TX_OFFLOAD_MBUF_FAST_FREE)
+    port_conf.txmode.offloads |=
+        DEV_TX_OFFLOAD_MBUF_FAST_FREE;
+  /* Force full Tx path in the driver, required for IEEE1588 */
+  port_conf.txmode.offloads |= DEV_TX_OFFLOAD_MULTI_SEGS;
 
-	/* Configure the Ethernet device. */
-	retval = rte_eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
-	if (retval != 0)
-		return retval;
+  /* Configure the Ethernet device. */
+  retval = rte_eth_dev_configure(port, rx_rings, tx_rings, &port_conf);
+  if (retval != 0)
+    return retval;
 
-	retval = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
-	if (retval != 0)
-		return retval;
+  retval = rte_eth_dev_adjust_nb_rx_tx_desc(port, &nb_rxd, &nb_txd);
+  if (retval != 0)
+    return retval;
 
-	/* Allocate and set up 1 RX queue per Ethernet port. */
-	for (q = 0; q < rx_rings; q++) {
-		retval = rte_eth_rx_queue_setup(port, q, nb_rxd,
-				rte_eth_dev_socket_id(port), NULL, mbuf_pool);
+  /* Allocate and set up 1 RX queue per Ethernet port. */
+  for (q = 0; q < rx_rings; q++)
+  {
+    retval = rte_eth_rx_queue_setup(port, q, nb_rxd,
+                                    rte_eth_dev_socket_id(port), NULL, mbuf_pool);
 
-		if (retval < 0)
+    if (retval < 0)
 
-			return retval;
-	}
+      return retval;
+  }
 
-	/* Allocate and set up 1 TX queue per Ethernet port. */
-	for (q = 0; q < tx_rings; q++) {
-		struct rte_eth_txconf *txconf;
+  /* Allocate and set up 1 TX queue per Ethernet port. */
+  for (q = 0; q < tx_rings; q++)
+  {
+    struct rte_eth_txconf *txconf;
 
-		txconf = &dev_info.default_txconf;
-		txconf->offloads = port_conf.txmode.offloads;
+    txconf = &dev_info.default_txconf;
+    txconf->offloads = port_conf.txmode.offloads;
 
-		retval = rte_eth_tx_queue_setup(port, q, nb_txd,
-				rte_eth_dev_socket_id(port), txconf);
-		if (retval < 0)
-			return retval;
-	}
+    retval = rte_eth_tx_queue_setup(port, q, nb_txd,
+                                    rte_eth_dev_socket_id(port), txconf);
+    if (retval < 0)
+      return retval;
+  }
 
-	/* Start the Ethernet port. */
-	retval = rte_eth_dev_start(port);
-	if (retval < 0)
-		return retval;
+  /* Start the Ethernet port. */
+  retval = rte_eth_dev_start(port);
+  if (retval < 0)
+    return retval;
 
-	/* Enable timesync timestamping for the Ethernet device */
-	retval = rte_eth_timesync_enable(port);
-	if (retval < 0) {
-		printf("Timesync enable failed: %d\n", retval);
-		return retval;
-	} else {
-		printf("Enabling Timestamp on port %d\n", port);
-	}
+  /* Enable timesync timestamping for the Ethernet device */
+  retval = rte_eth_timesync_enable(port);
+  if (retval < 0)
+  {
+    printf("Timesync enable failed: %d\n", retval);
+    return retval;
+  }
+  else
+  {
+    printf("Enabling Timestamp on port %d\n", port);
+  }
 
-	/* Enable RX in promiscuous mode for the Ethernet device. */
-	retval = rte_eth_promiscuous_enable(port);
-	if (retval != 0) {
-		printf("Promiscuous mode enable failed: %s\n",
-			rte_strerror(-retval));
-		return retval;
-	}
-	printf("Initialized port %d\n",port);
-	return 0;
+  /* Enable RX in promiscuous mode for the Ethernet device. */
+  retval = rte_eth_promiscuous_enable(port);
+  if (retval != 0)
+  {
+    printf("Promiscuous mode enable failed: %s\n",
+           rte_strerror(-retval));
+    return retval;
+  }
+  printf("Initialized port %d\n", port);
+  return 0;
 }
-
 
 /*
  * The lcore main. This is the main thread that does the work, reading from an
@@ -138,141 +141,147 @@ static __rte_noreturn void
 lcore_main(void)
 {
 
-	printf("\nStarting DPTP...\n");
+  printf("\nStarting DPTP...\n");
 
-	start_dptp(ptp_enabled_port_nb, tor_switch, mbuf_pool);
-
+  start_dptp(ptp_enabled_port_nb, tor_switch, mbuf_pool);
 }
 
 static void
 print_usage(const char *prgname)
 {
-	printf("%s [EAL options] -- -p PORTMASK \n"
-		" -p PORTMASK: hexadecimal bitmask of ports to configure\n",
-		prgname);
+  printf("%s [EAL options] -- -p PORTMASK \n"
+         " -p PORTMASK: hexadecimal bitmask of ports to configure\n",
+         prgname);
 }
 
 static int
 ptp_parse_portmask(const char *portmask)
 {
-	char *end = NULL;
-	unsigned long pm;
+  char *end = NULL;
+  unsigned long pm;
 
-	/* Parse the hexadecimal string. */
-	pm = strtoul(portmask, &end, 16);
+  /* Parse the hexadecimal string. */
+  pm = strtoul(portmask, &end, 16);
 
-	if ((portmask[0] == '\0') || (end == NULL) || (*end != '\0'))
-		return -1;
+  if ((portmask[0] == '\0') || (end == NULL) || (*end != '\0'))
+    return -1;
 
-	if (pm == 0)
-		return -1;
+  if (pm == 0)
+    return -1;
 
-	return pm;
+  return pm;
 }
 
 /* Parse the commandline arguments. */
 static int
 ptp_parse_args(int argc, char **argv)
 {
-	int opt;
-	char **argvopt;
-	int option_index;
-	char *prgname = argv[0];
-	static struct option lgopts[] = { {NULL, 0, 0, 0} };
+  int opt;
+  char **argvopt;
+  int option_index;
+  char *prgname = argv[0];
+  static struct option lgopts[] = {{NULL, 0, 0, 0}};
 
-	argvopt = argv;
+  argvopt = argv;
 
-	while ((opt = getopt_long(argc, argvopt, "p:",
-				  lgopts, &option_index)) != EOF) {
+  while ((opt = getopt_long(argc, argvopt, "p:",
+                            lgopts, &option_index)) != EOF)
+  {
 
-		switch (opt) {
+    switch (opt)
+    {
 
-		/* Portmask. */
-		case 'p':
-			ptp_enabled_port_mask = ptp_parse_portmask(optarg);
-			if (ptp_enabled_port_mask == 0) {
-				printf("invalid portmask\n");
-				print_usage(prgname);
-				return -1;
-			}
-			break;
-		default:
-			print_usage(prgname);
-			return -1;
-		}
-	}
+    /* Portmask. */
+    case 'p':
+      ptp_enabled_port_mask = ptp_parse_portmask(optarg);
+      if (ptp_enabled_port_mask == 0)
+      {
+        printf("invalid portmask\n");
+        print_usage(prgname);
+        return -1;
+      }
+      break;
+    default:
+      print_usage(prgname);
+      return -1;
+    }
+  }
 
-	argv[optind-1] = prgname;
+  argv[optind - 1] = prgname;
 
-	optind = 1; /* Reset getopt lib. */
+  optind = 1; /* Reset getopt lib. */
 
-	return 0;
+  return 0;
 }
 
 /*
  * The main function, which does initialization and calls the per-lcore
  * functions.
  */
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	unsigned nb_ports;
+  unsigned nb_ports;
 
-	uint16_t portid;
+  uint16_t portid;
 
-	/* Initialize the Environment Abstraction Layer (EAL). */
-	int ret = rte_eal_init(argc, argv);
+  /* Initialize the Environment Abstraction Layer (EAL). */
+  int ret = rte_eal_init(argc, argv);
 
-	if (ret < 0)
-		rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
+  if (ret < 0)
+    rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
 
+  argc -= ret;
+  argv += ret;
 
+  ret = ptp_parse_args(argc, argv);
+  if (ret < 0)
+    rte_exit(EXIT_FAILURE, "Error with PTP initialization\n");
 
-	argc -= ret;
-	argv += ret;
+  /* Check that there is an even number of ports to send/receive on. */
+  nb_ports = rte_eth_dev_count_avail();
 
-	ret = ptp_parse_args(argc, argv);
-	if (ret < 0)
-		rte_exit(EXIT_FAILURE, "Error with PTP initialization\n");
+  /* Creates a new mempool in memory to hold the mbufs. */
+  mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports,
+                                      MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
 
-	/* Check that there is an even number of ports to send/receive on. */
-	nb_ports = rte_eth_dev_count_avail();
+  if (mbuf_pool == NULL)
+    rte_exit(EXIT_FAILURE, "Cannot create mbuf pool\n");
 
-	/* Creates a new mempool in memory to hold the mbufs. */
-	mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports,
-		MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
+  /* Initialize all ports. */
+  RTE_ETH_FOREACH_DEV(portid)
+  {
+    printf("%d %d\n", portid, ptp_enabled_port_mask);
+    if ((ptp_enabled_port_mask & (1 << portid)) != 0)
+    {
+      if (port_init(portid, mbuf_pool) == 0)
+      {
+        ptp_enabled_ports[ptp_enabled_port_nb] = portid;
+        ptp_enabled_port_nb++;
+        printf("Enabled port %d\n", portid);
+      }
+      else
+      {
+        rte_exit(EXIT_FAILURE,
+                 "Cannot init port %" PRIu8 "\n",
+                 portid);
+      }
+    }
+    else
+      printf("Skipping disabled port %u\n", portid);
+  }
 
-	if (mbuf_pool == NULL)
-		rte_exit(EXIT_FAILURE, "Cannot create mbuf pool\n");
+  if (ptp_enabled_port_nb == 0)
+  {
+    rte_exit(EXIT_FAILURE,
+             "All available ports are disabled."
+             " Please set portmask.\n");
+  }
 
-	/* Initialize all ports. */
-	RTE_ETH_FOREACH_DEV(portid) {
-		printf("%d %d\n", portid, ptp_enabled_port_mask);
-		if ((ptp_enabled_port_mask & (1 << portid)) != 0) {
-			if (port_init(portid, mbuf_pool) == 0) {
-				ptp_enabled_ports[ptp_enabled_port_nb] = portid;
-				ptp_enabled_port_nb++;
-				printf("Enabled port %d\n", portid);
-			} else {
-				rte_exit(EXIT_FAILURE,
-					 "Cannot init port %"PRIu8 "\n",
-					 portid);
-			}
-		} else
-			printf("Skipping disabled port %u\n", portid);
-	}
+  if (rte_lcore_count() > 1)
+    printf("\nWARNING: Too many lcores enabled. Only 1 used.\n");
 
-	if (ptp_enabled_port_nb == 0) {
-		rte_exit(EXIT_FAILURE,
-			"All available ports are disabled."
-			" Please set portmask.\n");
-	}
+  /* Call lcore_main on the master core only. */
+  lcore_main();
 
-	if (rte_lcore_count() > 1)
-		printf("\nWARNING: Too many lcores enabled. Only 1 used.\n");
-
-	/* Call lcore_main on the master core only. */
-	lcore_main();
-
-	return 0;
+  return 0;
 }
